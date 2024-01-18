e59559c0aef46d6a6f9a29e66dbaff908b2bfd9e
priyam dwivedi <priyamdwivedi107@gmail.com>
2024-01-19 03:23:28 +0530

    updated

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -4,20 +4,20 @@
         </a></div>
       <div class="sidebar-wrapper">
         <ul class="nav">
-          <li class="nav-item active">
+          <li class="nav-item {{ Request::is('dashboard') ? 'active':'' }}">
               <a class="nav-link" href="{{ url ('dashboard') }}">
                 <i class="material-icons">dashboard</i>
                 <p>Dashboard</p>
               </a>
           </li>
 
-          <li class="nav-item ">
+          <li class="nav-item {{ Request::is('products') ? 'active':'' }}">
             <a class="nav-link" href="{{ url ('products') }}">
               <i class="material-icons">person</i>
               <p>Product</p>
             </a>
           </li>
-          <li class="nav-item ">
+          <li class="nav-item {{ Request::is('add-product') ? 'active':'' }}">
             <a class="nav-link" href="{{ url ('add-product') }}">
               <i class="material-icons">person</i>
               <p>Add Product</p>
